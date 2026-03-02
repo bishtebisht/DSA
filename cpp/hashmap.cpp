b6e994315acb5553a6dd1d1dc6059dcd749d1898
bishtebisht <bhoomibisht007@gmail.com>
2026-03-02 15:05:39 +0530

    ransom note

--- cpp/hashmap.cpp
+++ cpp/hashmap.cpp
@@ -112,3 +112,19 @@ bool isIsomorphic(string s, string t) {
     
     return true;
 }
+
+//ransomeNote
+bool canConstruct(string ransomNote, string magazine) {
+    unordered_map<char, int> mp;
+
+    for(char c : magazine){
+        mp[c]++;
+    }
+    for(char c : ransomNote) {
+        if(mp[c] == 0)
+            return false;
+        mp[c]--;
+    }
+
+    return true;
+}
